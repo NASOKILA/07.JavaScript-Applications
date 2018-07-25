f0ab8f03f03388937cfb3709670930baee5e7513
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 06:56:58 +0300

    Asynchronous Programming

--- 04.Asynchronous Programming/async&awaut.js
+++ 04.Asynchronous Programming/async&awaut.js
@@ -1,44 +1,23 @@
 
+async function test() {
 
-
-async function test(){
-
-   // throw new Error('Hello Error !');
-   
     console.log('Start ...')
 
-    //ZA DA POKAJEM AWAIT NI TRQBVA PROMISE KOITO DA SIMULIRA NQKAKVA ASINHROMNA ZAQVKA
-    await new Promise(function(resolve, reject){
-
-        //kazvame mu sled dve sekundi da resolvne 'Success !'
-        setTimeout(function(){
+    await new Promise(function (resolve, reject) {
+        setTimeout(function () {
             resolve('Success !');
-        },2000);
-
-    }).then(function(res){
-        console.log(res); //printirame 'Success !'
+        }, 2000);
+    }).then(function (res) {
+        console.log(res);
     })
 
-    //SLED KATO PRIKLUCHI GORNOTO CHAK TOGAVA IDVAME TUKA I VRUSHTAME 'End ...' !!!
-    //AKO MAHNEM 'await' SHTE PRINTIRA 'End ...' I POSLE 'Success !'
     return 'End ...';
 }
 
-test().then(function(res){
-    //sled kato ima 'async' to stava na 'Promise' i mojem da mu zakachame .then() i .catch()
+test().then(function (res) {
     console.log(res)
 })
-.catch(function(err){
-    console.log('ERRORRR !') //ako imame greshka vlizame tuka
-    console.log(err);
-})
-
-
-//AKO MAHNEM 'await' SHTE PRINTIRA 'End ...' I POSLE 'Success !'
-//ZASHTOTO NQMA DA IZCHAKA VUTRESHNIQ PROMISE,
-//async PREVRUSHTA NORMALNA FUNKCIQ V Promise I AKO ISKAME DA POLZVAME 'await' V DADEN PROMISE
-//NI TRQBVA 'async' OTPRED.
-
-
-
-
+    .catch(function (err) {
+        console.log('ERRORRR !')
+        console.log(err);
+    })
--- 04.Asynchronous Programming/solution.js
+++ 04.Asynchronous Programming/solution.js
@@ -2,17 +2,9 @@
 function attachEvents() {
 
     const URL = 'https://baas.kinvey.com/appdata/kid_BJKFMbN9z/';
-
-    //TUK SHTE DURPAME OT KINVEY BAZA I NI TRQBVA AUTENTIKACIQ.
-    //TRQBVA NI IME I PAROLA ZA VECHE DOBAVEN USER ZA DA VLEzEM
     const USERNAME = 'Peter';
     const PASSWORD = 'ppp';
-
-    //TRQBVA DA KESHIRAME USERNAME i PAROLATA PO SPECIQLEN NACHIN ZA KINVEY-to SUS BASE64.
-    //vij saita : www.base64.org 
     const BASE_64 = btoa(USERNAME + ':' + PASSWORD);
-
-    //PRAVIM SI SPECIALNIQ OBEKT KOITO SE OCHAKVA OT KENVEY I GO PODAVAME NA ZAQVKATA S KLUCH 'headers':
     const AUTH = { 'Authorization': 'Basic ' + BASE_64 };
 
     $('#btnLoadPosts').click(function () {
@@ -30,7 +22,6 @@ function attachEvents() {
 
                     let post = res[key];
                     let id = post._id;
-
                     let option = $(`<option value="${id}">${post.title}</option>`);
 
                     $('#posts').append(option);
@@ -39,16 +30,13 @@ function attachEvents() {
             .catch(function (err) {
                 console.log(err)
             });
-
     });
-
 
     $('#btnViewPost').click(function () {
 
         let selectedOption = $('#posts > option:selected')[0];
         let id = selectedOption.value;
 
-        //Post 1
         $.ajax({
 
             method: 'GET',
@@ -60,12 +48,6 @@ function attachEvents() {
                 let title = res.title;
                 let body = res.body;
 
-                /*
-                $('#post-title').text(title);
-                $('#post-body').text(body);
-                */
-
-                //comments
                 $.ajax({
                     method: 'GET',
                     headers: AUTH,
@@ -81,8 +63,7 @@ function attachEvents() {
 
                             $('#post-comments').append(li);
                         }
-                        
-                        //SLAGAM GI TUKA ZA DA SE ZARDAT ZAEDNO S KOMENTARITE
+
                         $('#post-title').text(title);
                         $('#post-body').text(body);
                     })
@@ -90,19 +71,9 @@ function attachEvents() {
                         console.log('ERROR')
                         console.log(err)
                     });
-
-
-
             })
             .catch(function (err) {
                 console.log(err)
             });
-
-
     });
-
-
-    //MOJEHME DA POLZVAME I Promise.all([]);
-
 }
-
