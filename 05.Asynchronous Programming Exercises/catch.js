21f9e2f8d714e41b72131bf0b3c089a90d696555
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 07:03:20 +0300

    Asynchronous Programming Exercises

--- 05.Asynchronous Programming Exercises/catch.js
+++ 05.Asynchronous Programming Exercises/catch.js
@@ -3,35 +3,26 @@ function attachEvents() {
 
     const appKey = "kid_ry9Ndeucf";
     const baseUrl = `https://baas.kinvey.com/appdata/${appKey}/biggestCatches`;
-
     const username = "guest";
     const password = "guest";
 
-    //keshirame s BASE_64 Encoding, KINVEY GO IZIZKVA TAKA
     let BASE_64 = 'Basic ' + btoa(username + ":" + password);
 
-    //Load Button
     $('.load').on('click', function () {
 
-        //load all catches
         $.ajax({
             method: 'GET',
             url: baseUrl,
-            headers: { 'Authorization': BASE_64 },  //TAKA GO ISKA KINVEY
+            headers: { 'Authorization': BASE_64 },
             success: function handleSuccess(res) {
-
 
                 $('#main').empty();
 
-                //triem vsichki
                 $('<div id="catches"></div>').appendTo('#main');
 
-                //za vseki ot bazata mu pravim eventi na butonite i go zakachame za #catches
                 for (let cat4 of res) {
 
                     let updateBtn = $(`<button class="update">Update</button>`).click(function () {
-
-                        //make a post request to create a Catch
 
                         let angler = $(this).parent().find('.angler').val();
                         let weight = Number($(this).parent().find('.weight').val());
@@ -39,46 +30,36 @@ function attachEvents() {
                         let location = $(this).parent().find('.location').val();
                         let bait = $(this).parent().find('.bait').val();
                         let captureTime = Number($(this).parent().find('.captureTime').val());
-
-                        //suzdavame si obekta koito shte ni sloji dannite v bazata
                         let catchObj = { angler, weight, species, location, bait, captureTime };
-
 
                         $.ajax({
                             method: 'PUT',
                             data: JSON.stringify(catchObj),
-                            url: baseUrl  + `/${cat4._id}`,
-                            headers: 
+                            url: baseUrl + `/${cat4._id}`,
+                            headers:
                             {
-                                'Content-Type' : 'application/json', 
-                                'Authorization': BASE_64 
+                                'Content-Type': 'application/json',
+                                'Authorization': BASE_64
                             },
                             success: function s(res) {
-                                //console.log(res);
                             },
                             error: function e(err) {
-                                //console.log(err);                
                             }
                         });
-
                     });
-
 
                     let deleteBtn = $(`<button class="delete">Delete</button>`).click(function () {
 
-                        //triem go ot bazata
                         $.ajax({
                             method: 'DELETE',
                             url: baseUrl + `/${cat4._id}/`,
                             headers: {
-                                'Content-Type' : 'application/json', 
-                                'Authorization': BASE_64 
+                                'Content-Type': 'application/json',
+                                'Authorization': BASE_64
                             },
                         })
                         $(this).parent().remove();
-
                     });
-
 
                     let template = $(
                         `<div class="catch" data-id="<id-goes-here>">
@@ -100,27 +81,20 @@ function attachEvents() {
                     template.append(deleteBtn);
 
                     $('#catches').append(template);
-
                 }
-
             },
             error: function handleError(res) {
                 console.log('ERROR')
                 console.log(res)
             }
-
         })
-
 
         $('.angler').val();
         $('.angler').val();
         $('.angler').val();
     })
 
-    //Add Button - Add a new catch
     $('.add').on('click', function () {
-
-        //make a post request to create a Catch
 
         let angler = $('#addForm .angler').val();
         let weight = Number($('#addForm .weight').val());
@@ -129,38 +103,26 @@ function attachEvents() {
         let bait = $('#addForm .bait').val();
         let captureTime = Number($('#addForm .captureTime').val());
 
-        //suzdavame si obekta koito shte ni sloji dannite v bazata
         let catchObj = { angler, weight, species, location, bait, captureTime };
 
-        //suzdavame si noviq catch
         $.ajax({
             method: 'POST',
             data: JSON.stringify(catchObj),
             url: baseUrl,
             headers: {
-                'Content-Type' : 'application/json', 
-                'Authorization': BASE_64 
+                'Content-Type': 'application/json',
+                'Authorization': BASE_64
             },
             success: function s(res) {
-                //console.log(res);
             },
             error: function e(err) {
-                //console.log(err);                
             }
         });
 
-
-        //triem poletata:
         $('#addForm .angler').val("");
         $('#addForm .weight').val("");
         $('#addForm .species').val("");
         $('#addForm .location').val("");
         $('#addForm .captureTime').val("");
-
     })
-
 }
-
-
-
-
--- 05.Asynchronous Programming Exercises/forecaster.js
+++ 05.Asynchronous Programming Exercises/forecaster.js
@@ -1,46 +1,40 @@
 
-function attachEvents()
-{
+function attachEvents() {
 
-    let weatherSymbols = 
+    let weatherSymbols =
     {
-        'Sunny' : '&#x2600;',
-        'Partly sunny' : '&#x26C5;',
-        'Overcast' : '&#x2601;',
-        'Rain' : '&#x2614;',
-        'Degrees' : '&#176;'
+        'Sunny': '&#x2600;',
+        'Partly sunny': '&#x26C5;',
+        'Overcast': '&#x2601;',
+        'Rain': '&#x2614;',
+        'Degrees': '&#176;'
     }
 
-    
     let degree = weatherSymbols.Degrees;
 
-    $('#submit').click(function(){
+    $('#submit').click(function () {
 
         $('#current').empty();
         $('#upcoming').empty();
-        
+
         let inputText = $('#location').val();
 
         $.ajax({
-            
-            method : 'GET',
-            url : 'https://judgetests.firebaseio.com/locations.json',
-            success : function s(res){
+
+            method: 'GET',
+            url: 'https://judgetests.firebaseio.com/locations.json',
+            success: function s(res) {
 
                 let currentLocation = res.filter(r => r.name === inputText)[0];
-                
-                if(currentLocation === "" || currentLocation === undefined)
-                {
-                    
+
+                if (currentLocation === "" || currentLocation === undefined) {
+
                     $('#current').empty();
                     $('#upcoming').empty();
-                    
-                    $('#current')                
+                    $('#current')
                         .append(`<div class="label">Current conditions</div>`)
-                    
                     $('#upcoming')
                         .append(`<div class="label">Three-day forecast</div>`)
-                    
                     $('#forecast')[0].style.display = 'block';
                     $('.label').text('Error')
 
@@ -48,67 +42,49 @@ function attachEvents()
                 }
 
                 let code = currentLocation.code;
-                
-                
-                //Conditions
-                $.get(`https://judgetests.firebaseio.com/forecast/today/${code}.json`)
-                    .then(function ss(res){
-                        
-                        $('#forecast')[0].style.display = 'block';
-                       
-                        let symbol = weatherSymbols.Sunny;
 
+                $.get(`https://judgetests.firebaseio.com/forecast/today/${code}.json`)
+                    .then(function ss(res) {
+
+                        $('#forecast')[0].style.display = 'block';
+
+                        let symbol = weatherSymbols.Sunny;
                         let conditionSpan = $(`<span class="condition"></span>`)
                             .append(`<span class="forecast-data">${res.name}</span>`)
                             .append(`<span class="forecast-data">${res.forecast.high}${degree}/${res.forecast.low}${degree}</span>`)
                             .append(`<span class="forecast-data">${res.forecast.condition}</span>`)
 
                         $('#current').empty();
-
-                        $('#current')                
+                        $('#current')
                             .append(`<div class="label">Current conditions</div>`)
                             .append(`<span class="condition symbol">${symbol}</span>`)
                             .append(conditionSpan);
-                        
                     })
-                    
-                
-                //3 day forecast
+
                 $.get(`https://judgetests.firebaseio.com/forecast/upcoming/${code}.json`)
-                .then(function sss(res){
+                    .then(function sss(res) {
 
-                    
-                    $('#upcoming').empty();
-                    
-                    $('#upcoming')
-                    .append(`<div class="label">Three-day forecast</div>`)
-                            
-                    for (let obj of res.forecast) {
-
-                        let symbol = weatherSymbols[`${obj.condition}`]
-                        
-                        let spanUpcoming = $(`<span class="upcoming"></span>`)
-                            .append(`<span class="symbol">${symbol}</span>`)
-                            .append(`<span class="forecast-data">${obj.high}${degree}/${obj.low}${degree}</span>`)
-                            .append(`<span class="forecast-data">${obj.condition}</span>`)
-                        
+                        $('#upcoming').empty();
                         $('#upcoming')
-                            .append(spanUpcoming);
-                        
-                    }
+                            .append(`<div class="label">Three-day forecast</div>`)
 
-                })
+                        for (let obj of res.forecast) {
 
+                            let symbol = weatherSymbols[`${obj.condition}`]
+                            let spanUpcoming = $(`<span class="upcoming"></span>`)
+                                .append(`<span class="symbol">${symbol}</span>`)
+                                .append(`<span class="forecast-data">${obj.high}${degree}/${obj.low}${degree}</span>`)
+                                .append(`<span class="forecast-data">${obj.condition}</span>`)
 
+                            $('#upcoming')
+                                .append(spanUpcoming);
+                        }
+                    })
             },
-            error : function f(err){
+            error: function f(err) {
                 console.log('ERROR!');
                 console.log(err);
             }
-
-        });    
-
-    
+        });
     })
-
 }
