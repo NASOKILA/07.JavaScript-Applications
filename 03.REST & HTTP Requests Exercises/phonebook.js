cbde41735c7dd7cd5553dd43cd82386a211a2013
Atanas Kambitov <atanasskambitov@gmail.com>
2018-07-25 06:51:52 +0300

    REST & HTTP Requests Exercises

--- 03.REST & HTTP Requests Exercises/phonebook.js
+++ 03.REST & HTTP Requests Exercises/phonebook.js
@@ -1,33 +1,30 @@
 
-function attachEvents()
-{
-    $('#btnLoad').click(function(){
-        
+function attachEvents() {
+    $('#btnLoad').click(function () {
+
         $.ajax({
             method: 'GET',
-            url : 'https://phonebook-d76c9.firebaseio.com/phonebook.json',
-            success: function success(res){
-                
+            url: 'https://phonebook-d76c9.firebaseio.com/phonebook.json',
+            success: function success(res) {
 
                 $('#phonebook').empty();
 
-                //console.log('SUCCESS');
                 for (let id in res) {
-                    
+
                     let args = res[id];
                     let name = args.name;
                     let phone = args.phone;
 
                     let deleteBtn = $('<button>[Delete]</button>')
-                        .click(function(){
+                        .click(function () {
 
                             $.ajax({
-                                method : 'DELETE',
-                                url : `https://phonebook-d76c9.firebaseio.com/phonebook/${id}.json`,
-                                success : function s(res){
+                                method: 'DELETE',
+                                url: `https://phonebook-d76c9.firebaseio.com/phonebook/${id}.json`,
+                                success: function s(res) {
                                     console.log('Success');
                                 },
-                                error : function e(err){
+                                error: function e(err) {
                                     console.log('ERROR');
                                 }
                             })
@@ -37,21 +34,18 @@ function attachEvents()
 
                     let li = $(`<li>${name}: ${phone}</li>`)
                         .append(deleteBtn);
-                    
+
                     $('#phonebook').append(li);
                 }
-
-
             },
-            error: function error(err)
-            {
+            error: function error(err) {
                 console.log('ERROR');
             }
         });
 
     });
 
-    $('#btnCreate').click(function(){
+    $('#btnCreate').click(function () {
 
         let person = $('#person').val();
         let phone = $('#phone').val();
@@ -60,18 +54,18 @@ function attachEvents()
         $('#phone').val("");
 
         $.ajax({
-            method:'POST',
+            method: 'POST',
             url: 'https://phonebook-d76c9.firebaseio.com/phonebook.json',
-            data: JSON.stringify({'name':person, 'phone':phone}),
-            success : function success(res){
-                
+            data: JSON.stringify({ 'name': person, 'phone': phone }),
+            success: function success(res) {
+
                 let id = res.name;
-                
+
                 let deleteBtn = $('<button>[Delete]</button>');
-                deleteBtn.click(function(){
+                deleteBtn.click(function () {
                     $.ajax({
-                        method:'DELETE',
-                        url:`https://phonebook-d76c9.firebaseio.com/phonebook/${id}.json`,
+                        method: 'DELETE',
+                        url: `https://phonebook-d76c9.firebaseio.com/phonebook/${id}.json`,
                     });
 
                     $(this).parent().remove();
@@ -81,24 +75,12 @@ function attachEvents()
                 li.append(deleteBtn);
 
                 $('#phonebook').append(li);
-            
+
             },
-            error : function error(err){
+            error: function error(err) {
                 console.log('ERROR');
             }
         })
 
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
